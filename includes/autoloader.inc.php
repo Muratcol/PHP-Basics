2e91c59dcf3dfa570ea5f3dda29db012a2e307d6
Muratcol <muratcolyaran@yahoo.com.tr>
2020-10-03 18:07:18 +0300

    test04

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -1,6 +1,7 @@
 <?php
     //This is trick of load all classes automatically when you call them.
 
+    // Vers01 simpler way to auto-load classes to project
     spl_autoload_register('myAutoLoader');
     function myAutoLoader($className){
       
@@ -8,4 +9,18 @@
     }
 
 
+    //vers02 advanced way to auto-load classes to project
+    spl_autoload_register('myAutoLoader2');
+    function myAutoLoader2($className){
+        $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+        if(strpos($url, 'includes') !== false){
+            $path = '../classes/';
+        }
+        else{
+            $path = './classes/';
+        }
+        $extension = '.class.php';
+        require_once $path.$className.$extension;
+    }
 ?>
